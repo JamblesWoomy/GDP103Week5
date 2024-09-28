3ce2a9328758c1636f1a750f5df031f1a82a5197
JamblesWoomy <jmcampbell1309@gmail.com>
2024-09-28 14:34:36 +1000

    Added ding ding things

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class Player : MonoBehaviour
 {
     private Animator animator;
     // Start is called before the first frame update
     private Vector2 moveInput;
     private CharacterController characterController;
-    public float moveSpeed = 5f;
+    private Vector3 direction;
 
-    void Start()
+    [SerializeField] private float smoothTime = 0.05f;
+    private float currentVelocity;
+
+    [SerializeField] private float moveSpeed;
+
+    private float gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 3.0f;
+    private float velocity;
+
+    void Awake()
     {
         // Get the Animator component attached to the same GameObject
         animator = GetComponent<Animator>();
@@ -17,28 +27,62 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y) *
+        /*Vector3 move = new Vector3(moveInput.x, 0, moveInput.y) *
         moveSpeed * Time.deltaTime;
-        characterController.Move(move);
-        // Update the animator with the movement speed
+        characterController.Move(move);*/
+        // Update the animator with the movement speeeeeee
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
         animator.SetFloat("velocityX", moveInput.x * moveSpeed);
         animator.SetFloat("velocityY", moveInput.y * moveSpeed);
     }
+    private void ApplyGravity()
+    {
+        if (characterController.isGrounded)
+        {
+            velocity = -1.0f;
+        }
+        else
+        {
+            velocity += gravity * gravityMultiplier * Time.deltaTime;
+        }
+        direction.y = velocity;
+    }
+
+    private void ApplyRotation()
+    {
+        if (moveInput.sqrMagnitude == 0)
+        {
+            animator.SetFloat("velocityX", 0);
+            animator.SetFloat("velocityY", 0);
+            return;
+        }
+        var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentVelocity, smoothTime);
+        transform.rotation = Quaternion.Euler(0.0f, targetAngle, 0.0f);
+    }
+    private void ApplyMovement()
+    {
+        characterController.Move(direction * moveSpeed * Time.deltaTime);
+    }
+
     // Method to handle the Clap action started
-    public void OnFall(InputAction.CallbackContext context)
+    public void OnFlair(InputAction.CallbackContext context)
     {
         if (context.started)
         {
-            animator.SetBool("fall", true);
+            animator.SetBool("Flair", true);
         }
         else if (context.canceled)
         {
-            animator.SetBool("fall", false);
+            animator.SetBool("Flair", false);
         }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+        direction = new Vector3(moveInput.x, 0.0f, moveInput.y);
     }
 }
