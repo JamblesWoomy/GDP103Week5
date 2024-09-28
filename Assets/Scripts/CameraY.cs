48b35c2db4dbe3ce667ce1c14188fc6db91968b5
JamblesWoomy <jmcampbell1309@gmail.com>
2024-09-28 20:47:21 +1000

    Fixed up camera controls

--- Assets/Scripts/CameraY.cs
+++ Assets/Scripts/CameraY.cs
@@ -6,15 +6,9 @@ public class CameraY : MonoBehaviour
 {
 
     public GameObject Reciever;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, Reciever.transform.position.y + 2f, transform.position.z);
+        transform.position = new Vector3(Reciever.transform.position.x, transform.position.y, transform.position.z);
     }
 }
